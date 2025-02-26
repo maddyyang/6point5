7aa6425b168210177c4617c01c834e280c56c3d1
maddyyang <131908833+maddyyang@users.noreply.github.com>
2025-02-26 17:08:37 +0000

    ..

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,12 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  
+  return {
+    dir: {
+      input: "src",      
+      includes: "_includes",
+      output: "dist"     
+    }
+  };
 };
